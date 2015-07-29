00a0eccc66fd0300d3feabcd1d51e3724fafff9a
rasiquiz <rasiquiz@gmail.com>
2015-07-29 20:48:24 +0200

    Added Prism libraries

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projects</title>
+	<link rel="stylesheet"  type="text/css" href="style.css"/>
+	<link rel="stylesheet" type="text/css" href="Prism.css" />
+	<script src="Prism.js"></script>
+</head>
+<body>
+	<div id="con">
+		<h1 class="text" style="font-size:30pt;">Projects</h1>
+		<h3 class="text">by rasiquiz</h3>
+		<br>
+		<pre><code class="language-javascript">setTimeout(function(){document.getElementsByClassName('titletext' + id)[0].style.marginLeft = '-' + $('.titletext' + id).innerWidth()/2 + 'px'}, 0)</code></pre>
+		<pre><code class="language-css">.p { font: 100% 'Helvetica'; top: 10px; background-color: #123456; }</code></pre>
+		<pre><code class="language-markup"><?php htmlspecialchars("<p class="text" style="font-size:30pt;">Projects</p>");?></code></pre>
+	</div>
+</body>
+</html>
